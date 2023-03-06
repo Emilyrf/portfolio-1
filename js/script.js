863c06a27d588b762016f900a63b3e18942eea16
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-03-06 13:44:06 +0100

    Getting the site live on netlify

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//Hamburguer menu
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+});
+
+//Active link
+
+
+//Sticky position
+window.onscroll = function() {stickyFunction()};
+
+var navbar = document.querySelector("header");
+var sticky = navbar.offsetTop;
+
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
